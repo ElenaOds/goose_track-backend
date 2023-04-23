cef0197f346e6b51148707f8b1f49301105639a2
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2023-04-23 19:01:02 +0300

    add infoController

--- controllers/index.js
+++ controllers/index.js
@@ -2,21 +2,21 @@ const { registrationController } = require("./users/registrationController");
 const { loginController } = require("./users/loginController");
 const { currentUserController } = require("./users/currentUserController");
 const { logoutController } = require("./users/logoutController");
+const { infoController } = require("./users/infoController");
 
 // const { uploadAvatarController } = require("./uploadAvatarController");
 // const { verificationController } = require("./verificationController");
 // const { resendingController } = require("./resendingController");
 
-
 module.exports = {
-    registrationController,
-    loginController,
-    currentUserController,
-    logoutController
+  registrationController,
+  loginController,
+  currentUserController,
+  logoutController,
+  infoController,
 
-    
-    // currentUserController,
-    // uploadAvatarController,
-    // verificationController,
-    // resendingController
-}
+  // currentUserController,
+  // uploadAvatarController,
+  // verificationController,
+  // resendingController
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,19 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = new express.Router();
 
-const { asyncWrapper } = require('../../helpers/index');
-const { authMiddleware } = require('../../middlewares/index');
-const {currentUserController, logoutController} = require('../../controllers/index');
+const { asyncWrapper } = require("../../helpers/index");
+const { authMiddleware } = require("../../middlewares/index");
+const {
+  currentUserController,
+  logoutController,
+  infoController,
+} = require("../../controllers/index");
 
+router.get("/current", authMiddleware, asyncWrapper(currentUserController));
+router.post("/logout", authMiddleware, asyncWrapper(logoutController));
+router.patch("/info", authMiddleware, asyncWrapper(infoController));
 
-router.get('/current', authMiddleware, asyncWrapper(currentUserController));
-router.post('/logout', authMiddleware, asyncWrapper(logoutController));
-
-
-
-
-
-
-
-module.exports = {usersRouter: router};
-
+module.exports = { usersRouter: router };
