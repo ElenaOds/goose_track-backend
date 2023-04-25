f41093e2cd8955927527c84fbb4c217f835f145d
KarlAnna <106392024+KarlAnna@users.noreply.github.com>
2023-04-25 13:43:45 +0300

    Add usePhotos upload

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const { registrationController } = require("./users/registrationController");
 const { loginController } = require("./users/loginController");
 const { currentUserController } = require("./users/currentUserController");
 const { logoutController } = require("./users/logoutController");
-const { infoController } = require("./users/infoController");
+const { updateInfoController } = require("./users/updateInfoController");
 
 // const { uploadAvatarController } = require("./uploadAvatarController");
 // const { verificationController } = require("./verificationController");
@@ -13,7 +13,7 @@ module.exports = {
   loginController,
   currentUserController,
   logoutController,
-  infoController,
+  updateInfoController,
 
   // currentUserController,
   // uploadAvatarController,
---
+++ controllers/users/updateInfoController.js
@@ -0,0 +1,48 @@
+const { User } = require("../../service/users/userSchema");
+const path = require("path");
+const fs = require("fs/promises");
+const Jimp = require("jimp");
+const { error } = require("console");
+
+const userPhotosDir = path.join(__dirname, "../../", "public", "userPhotos");
+
+module.exports = {
+  updateInfoController: async (req, res) => {
+    const { _id } = req.user;
+    const { name, email, phone, birthday, skype } = req.body;
+    const { path: tempUpload, originalname } = req.file;
+    const imgName = `${_id}_${originalname}`;
+    try {
+      const resultUpload = path.join(userPhotosDir, imgName);
+      Jimp.read(tempUpload, (error, img) => {
+        if (error) throw error;
+        img.resize(124, 124).quality(60).write(resultUpload);
+      });
+      await fs.rename(tempUpload, resultUpload);
+      const userPhotoURL = path.join("public", "userPhotos", imgName);
+      await User.findByIdAndUpdate(_id, {
+        name,
+        email,
+        phone,
+        birthday,
+        skype,
+        userPhoto: userPhotoURL,
+      });
+
+      res.status(200).json({
+        msg: "Update successful",
+        user: {
+          name,
+          email,
+          phone,
+          birthday,
+          skype,
+          userPhoto: userPhotoURL,
+        },
+      });
+    } catch (error) {
+      await fs.unlink(tempUpload);
+      throw error;
+    }
+  },
+};
